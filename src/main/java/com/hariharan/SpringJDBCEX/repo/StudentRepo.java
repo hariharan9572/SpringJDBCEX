8eca8950c9ddb75809cc05d6601ea200b86153b4
Hariharan <hariharan9572@gmail.com>
2026-01-09 12:28:15 +0530

    rowMapper

--- src/main/java/com/hariharan/SpringJDBCEX/repo/StudentRepo.java
+++ src/main/java/com/hariharan/SpringJDBCEX/repo/StudentRepo.java
@@ -3,8 +3,11 @@ package com.hariharan.SpringJDBCEX.repo;
 import com.hariharan.SpringJDBCEX.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,8 +36,22 @@ public class StudentRepo {
 
     public List<Student> findAll() {
 
-        List<Student> students = new ArrayList<>();
-        return  students;
+        String sql = "select * from student";
+
+        RowMapper<Student> mapper = new RowMapper<Student>() {
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+                Student s = new Student();
+                s.setRollNo(rs.getInt("rollno"));
+                s.setName((rs.getString("name")));
+                s.setMarks((rs.getInt("marks")));
+
+                return s;
+            }
+        };
+
+        return jdbc.query(sql, mapper);
 
     }
 }
