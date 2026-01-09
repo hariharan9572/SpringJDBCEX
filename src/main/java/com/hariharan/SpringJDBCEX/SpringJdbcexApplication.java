0e6dba7352af60e9cd18d67cf292515e7946bda1
Hariharan <hariharan9572@gmail.com>
2026-01-09 11:32:50 +0530

    first commit

---
+++ src/main/java/com/hariharan/SpringJDBCEX/SpringJdbcexApplication.java
@@ -0,0 +1,13 @@
+package com.hariharan.SpringJDBCEX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hariharan/SpringJDBCEX/SpringJdbcexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hariharan.SpringJDBCEX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
